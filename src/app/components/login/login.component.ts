5d84663dc54d033e45fc26311e4eab3b9d38cd2e
Austinys <austeja.zasimauskaite@gmail.com>
2022-04-06 16:52:49 +0300

    Bootstrap styles to login form added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,35 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-
 export class LoginComponent implements OnInit {
   loginForm: FormGroup = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
   });
-  
-  constructor(private formBuilder: FormBuilder) { }
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.loginForm = this.formBuilder.group(
-      {
-        email: ['', [Validators.required]],
-        password: ['', [Validators.required]]
-      },
-    );
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
   }
 
-  onSubmit(){}
-
+  onSubmit() {}
 }
-
- 
-
