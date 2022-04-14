2e9a57e44770291095d36f11210d8bba94dcf2de
Austinys <austeja.zasimauskaite@gmail.com>
2022-04-14 15:58:55 +0300

    toggle fixed another way

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public isMenuCollapsed = true;
+
   constructor() {}
 
   ngOnInit(): void {}
