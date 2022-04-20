200d5d1a3d5830fc8917bb47265e2e813c3393e8
Mantas <mantas.ambrazaitis9@gmail.com>
2022-04-20 22:55:17 +0300

    Restrictions to pages if not logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { OverviewComponent } from './overview/overview.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DeskBookingComponent } from './desk-booking/desk-booking.component';
+import {AuthGuard} from "./auth/auth.guard";
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
-  { path: 'overview', component: OverviewComponent },
-  { path: 'booking', component: DeskBookingComponent },
+  { path: 'overview', component: OverviewComponent, canActivate:[AuthGuard] },
+  { path: 'booking', component: DeskBookingComponent, canActivate:[AuthGuard] },
   { path: '**', component: PageNotFoundComponent },
 ];
 
