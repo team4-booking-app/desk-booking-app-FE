d24b013251ee878123ed9e3331ef55aeb0a53382
Austinys <austeja.zasimauskaite@gmail.com>
2022-04-20 16:15:56 +0300

    minor fix

--- src/app/desk-booking/booking.service.spec.ts
+++ src/app/desk-booking/booking.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { BookingService } from './booking.service';
 
-describe('BookingServiceService', () => {
+describe('BookingService', () => {
   let service: BookingService;
 
   beforeEach(() => {
