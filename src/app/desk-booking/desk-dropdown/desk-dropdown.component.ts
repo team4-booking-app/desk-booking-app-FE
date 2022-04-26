641b659da4854f44a3ae9240efb30440d942912d
Austinys <austeja.zasimauskaite@gmail.com>
2022-04-26 10:58:09 +0300

    work in progress

--- src/app/desk-booking/desk-dropdown/desk-dropdown.component.ts
+++ src/app/desk-booking/desk-dropdown/desk-dropdown.component.ts
@@ -4,11 +4,20 @@ import {
   Input,
   OnChanges,
   SimpleChanges,
+  forwardRef,
 } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
 import { BookingService } from '../booking.service';
 import { DateTimePickerComponent } from '../date-time-picker/date-time-picker.component';
 import { RoomDropdownComponent } from '../room-dropdown/room-dropdown.component';
+import {
+  NG_VALUE_ACCESSOR,
+  FormGroup,
+  FormBuilder,
+  ControlValueAccessor,
+  FormControl
+} from '@angular/forms';
+
 
 export interface Desks {
   deskId: number;
@@ -20,18 +29,59 @@ export interface Desks {
   selector: 'app-desk-dropdown',
   templateUrl: './desk-dropdown.component.html',
   styleUrls: ['./desk-dropdown.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DeskDropdownComponent),
+      multi: true
+    }]
 })
 export class DeskDropdownComponent implements OnInit, OnChanges {
+  deskDropdownForm: FormGroup;
   filteredDesks: any;
   selectedDesk: number;
+  subscriptions: Subscription[] = [];
 
   @Input() desks: any[];
   @Input() roomOpt: any;
+  value: any;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
+    this.deskDropdownForm = this.formBuilder.group({
+      deskName: ['']
+    })
+
     this.selectedDesk = 0;
     this.desks = [];
     this.filteredDesks = [];
+
+    this.subscriptions.push(
+      this.deskDropdownForm.valueChanges.subscribe(value => {
+        this.onChange(value);
+        this.onTouched();
+      })
+    );
+  }
+
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  writeValue(value: any) {
+    if (value) {
+      this.value = value;
+    }
+
+    if (value === null) {
+      this.deskDropdownForm.reset();
+    }
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
   }
 
   ngOnInit(): void {}
@@ -48,6 +98,10 @@ export class DeskDropdownComponent implements OnInit, OnChanges {
 
   getSelectedDesk() {
     return this.selectedDesk;
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 }
 
--- src/app/desk-booking/room-dropdown/room-dropdown.component.ts
+++ src/app/desk-booking/room-dropdown/room-dropdown.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-room-dropdown',
@@ -6,12 +7,17 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./room-dropdown.component.scss'],
 })
 export class RoomDropdownComponent implements OnInit {
+ roomDropdownForm: FormGroup;
   selectedRoom: number;
+
 
   @Output() redirectRoom: EventEmitter<any> = new EventEmitter();
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
     this.selectedRoom = 0;
+    this.roomDropdownForm = this.formBuilder.group({
+      roomName: ['']
+    })
   }
 
   ngOnInit(): void {}
